d9f91eb25422a99ebb56b2132abf5d42e27ba0c4
Ayush Verma <ayushpatel0375@gmail.com>
2026-05-24 00:16:10 +0530

    Add files via upload

---
+++ src/pages/Login.jsx
@@ -0,0 +1,77 @@
+import { useState } from "react";
+import axios from "axios";
+
+function Login() {
+  const [formData, setFormData] = useState({
+    email: "",
+    password: "",
+  });
+
+  const handleChange = (e) => {
+    setFormData({
+      ...formData,
+      [e.target.name]: e.target.value,
+    });
+  };
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+
+    try {
+      const res = await axios.post(
+        "http://localhost:5000/api/auth/login",
+        formData
+      );
+
+      localStorage.setItem(
+        "token",
+        res.data.token
+      );
+
+      localStorage.setItem(
+        "user",
+        JSON.stringify(res.data.user)
+      );
+
+      alert("Login Successful 🚀");
+
+      console.log(res.data);
+
+    } catch (error) {
+      console.log(error);
+      alert("Login Failed");
+    }
+  };
+
+  return (
+    <div style={{ padding: "30px" }}>
+      <h1>Login</h1>
+
+      <form onSubmit={handleSubmit}>
+        <input
+          type="email"
+          name="email"
+          placeholder="Enter Email"
+          onChange={handleChange}
+        />
+
+        <br /><br />
+
+        <input
+          type="password"
+          name="password"
+          placeholder="Enter Password"
+          onChange={handleChange}
+        />
+
+        <br /><br />
+
+        <button type="submit">
+          Login
+        </button>
+      </form>
+    </div>
+  );
+}
+
+export default Login;
